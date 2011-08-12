5cfae1e4d8d0b381d218e7852ac0cf4184b267a1
papricek <patrikjira@gmail.com>
2011-08-12 13:42:58 +0200

    Added some other constants to config

--- lib/gopay/config.rb
+++ lib/gopay/config.rb
@@ -14,12 +14,14 @@ module GoPay
 
   class Configuration
     attr_accessor :environment, :goid
-    attr_reader :country_codes
+    attr_reader :country_codes, :messages
 
     def initialize
       @environment ||= :production
       @country_codes = YAML.load_file File.join(BASE_PATH, "config", "country_codes.yml")
-      @urls = YAML.load_file(File.join(BASE_PATH, "config", "config.yml"))
+      config = YAML.load_file(File.join(BASE_PATH, "config", "config.yml"))
+      @urls = config["urls"]
+      @messages = config["messages"]
     end
 
     def urls
