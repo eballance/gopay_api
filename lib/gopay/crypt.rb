1b703dd5884b416d18c180dcb0305cca2619433e
papricek <patrikjira@gmail.com>
2011-08-14 19:26:45 +0200

    Also decryption functional

--- lib/gopay/crypt.rb
+++ lib/gopay/crypt.rb
@@ -19,6 +19,19 @@ module GoPay
       result.unpack("H*").to_s
     end
 
+    def decrypt(encrypted_data)
+      encrypted_data = bin2hex(encrypted_data)
+      des = OpenSSL::Cipher::Cipher.new("des-ede3")
+      des.decrypt
+      des.key = GoPay.configuration.secret
+      des.padding = 0
+      result = ""
+      result << des.update(encrypted_data)
+    end
+
+    def bin2hex(bin)
+      bin.scan(/../).map { | tuple | tuple.hex.chr }.to_s
+    end
   end
 
 end
--- test/crypt_test.rb
+++ test/crypt_test.rb
@@ -6,8 +6,8 @@ class CryptTest < Test::Unit::TestCase
 
     setup do
       @payment_command = GoPay::PaymentCommand.new({:variable_symbol => "gopay_test_#{GoPay.configuration.goid}",
-                                                           :total_price_in_cents => 100,
-                                                           :product_name => "productName"})
+                                                    :total_price_in_cents => 100,
+                                                    :product_name => "productName"})
     end
 
     should "generate sha1 hexdigest for an object" do
@@ -18,6 +18,11 @@ class CryptTest < Test::Unit::TestCase
     should "generate encrypted signature for an object" do
       assert_equal "0cb8bc2479744ffab52b828df43bf27cd0e73c2b128c21863ff39585a98d2195c49fdee291ec44df",
                    GoPay::Crypt.encrypt(@payment_command)
+    end
+
+    should "decrypt self encrypted signature" do
+      encrypted_stuff = GoPay::Crypt.encrypt(@payment_command)
+      assert_equal GoPay::Crypt.sha1(@payment_command.concat), GoPay::Crypt.decrypt(encrypted_stuff)
     end
 
   end
