116c514c2237dfefdb69383c813ef101e1aedbe8
papricek <patrikjira@gmail.com>
2011-08-14 17:24:16 +0200

    Setting only enviromnent in test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,10 +8,6 @@ require "pp"
 
 GoPay.configure do |config|
   config.environment = :test
-  config.goid = "8531903182"
-  config.success_url = "http://example.com/success"
-  config.failed_url = "http://example.com/failed"
-  config.secret = "a7ksDBye2Pm9sEwx3PfLiV6E"
 end
 
 GoPay.configure_from_yaml(File.join(File.dirname(__FILE__), "test_config.yml"))
