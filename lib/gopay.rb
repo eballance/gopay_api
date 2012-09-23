63a9666f3a97a64d124aa73688e2b59649407b6a
Patrik Jira <patrikjira@gmail.com>
2012-09-23 23:04:22 +0200

    Require only existing files and raising

--- lib/gopay.rb
+++ lib/gopay.rb
@@ -3,13 +3,7 @@ require "pp"
 require "gopay/config"
 require "gopay/crypt"
 
-require "gopay/models/payment"
-require "gopay/models/eshop_payment"
-require "gopay/models/customer_eshop_payment"
-require "gopay/models/buyer_payment"
-require "gopay/models/payment_identity"
 require "gopay/models/payment_method"
-
 require "gopay/models/base_payment"
 
 require "gopay/railtie" if defined?(::Rails) && ::Rails::VERSION::MAJOR >= 3
