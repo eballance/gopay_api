85b262878c94014a57bb65a7c311f8c2ffba0ebb
Pepan <chmel@jchsoft.cz>
2012-11-20 16:21:26 +0100

    opravy v modulu Crypt

--- lib/gopay/crypt.rb
+++ lib/gopay/crypt.rb
@@ -3,35 +3,37 @@ require "openssl"
 
 module GoPay
 
-  module Crypt
-    extend self
+	module Crypt
+		extend self
 
-    def sha1(string)
-      Digest::SHA1.hexdigest(string)
-    end
+		def sha1(string)
+			Digest::SHA1.hexdigest(string)
+		end
 
-    def encrypt(string)
-      string = sha1(string)
-      des = OpenSSL::Cipher::Cipher.new("des-ede3")
-      des.encrypt
-      des.key = GoPay.configuration.secure_key
-      result = des.update(string)
-      result.unpack("H*")[0]
-    end
+		def encrypt(string)
+			string = sha1(string)
+			des = OpenSSL::Cipher::Cipher.new("des-ede3")
+			des.encrypt
+			des.key = GoPay.configuration.secure_key
+			result = des.update(string)
+			result.unpack("H*")[0]
+		end
 
-    def decrypt(encrypted_data, padding_off = false)
-      encrypted_data = bin2hex(encrypted_data)
-      des = OpenSSL::Cipher::Cipher.new("des-ede3")
-      des.decrypt
-      des.padding = 0 if padding_off
-      des.key = GoPay.configuration.secure_key
-      result = ""
-      result << des.update(encrypted_data)
-    end
+		def decrypt(encrypted_data, padding_off = false)
+			encrypted_data = bin2hex(encrypted_data)
+			des = OpenSSL::Cipher.new("des-ede3")
+			des.decrypt
+			des.padding = 0 if padding_off
+			des.key = GoPay.configuration.secure_key
+			result = ""
+			result << des.update(encrypted_data)
+			result
+		end
 
-    def bin2hex(bin)
-      bin.scan(/../).map { | tuple | tuple.hex.chr }.to_s
-    end
-  end
+		def bin2hex(bin)
+			bin.scan(/../).map { |tuple| tuple.hex }.pack 'c*'
+		end
+
+	end
 
 end
