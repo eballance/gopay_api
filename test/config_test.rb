d05d3511aaad09134119513ef13cd436d49c4abf
Patrik Jira <patrikjira@gmail.com>
2012-11-21 20:21:45 +0100

    Fixed spec for new urls
    Url changed in 789719b

--- test/config_test.rb
+++ test/config_test.rb
@@ -7,7 +7,7 @@ class ConfigTest < Test::Unit::TestCase
 
     should "load both config and country_codes yml files" do
       assert_equal "Česká Republika", GoPay.configuration.country_codes["CZE"]
-      assert_equal "https://testgw.gopay.cz/gw/pay-full-v2", GoPay.configuration.urls["full_integration"]
+      assert_equal "http://testgw.gopay.cz/gw/pay-full-v2", GoPay.configuration.urls["full_integration"]
     end
 
   end
