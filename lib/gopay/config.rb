9078dfd6dad33a5fde952e5f874e6501833a9502
Eda Riedl <eduardriedl@gmail.com>
2015-01-25 17:19:55 +0100

    Better environment settings.

--- lib/gopay/config.rb
+++ lib/gopay/config.rb
@@ -1,14 +1,20 @@
 require "yaml"
 
 module GoPay
-
   BASE_PATH = File.expand_path("../../../", __FILE__)
-  STATUSES = {:created => "CREATED", :payment_method_chosen => "PAYMENT_METHOD_CHOSEN",
-              :paid => "PAID", :authorized => "AUTHORIZED",
-              :canceled => "CANCELED", :timeouted => "TIMEOUTED",
-              :refunded => "REFUNDED", :failed => "FAILED",
-              :call_completed => "CALL_COMPLETED", :call_failed => "CALL_FAILED",
-              :unknown => "UNKNOWN"}
+  STATUSES = {
+    created:    "CREATED", 
+    payment_method_chosen: "PAYMENT_METHOD_CHOSEN",
+    paid:       "PAID", 
+    authorized: "AUTHORIZED",
+    canceled:   "CANCELED", 
+    timeouted:  "TIMEOUTED",
+    refunded:   "REFUNDED", 
+    failed:     "FAILED",
+    call_completed: "CALL_COMPLETED", 
+    call_failed:    "CALL_FAILED",
+    unknown:        "UNKNOWN"
+  }
 
   def self.configure
     yield configuration
@@ -21,34 +27,38 @@ module GoPay
   def self.configure_from_yaml(path)
     yaml = YAML.load_file(path)
     return unless yaml
-    configuration.goid = yaml["goid"]
+    
+    configuration.goid        = yaml["goid"]
     configuration.success_url = yaml["success_url"]
-    configuration.failed_url = yaml["failed_url"]
-    configuration.secure_key = yaml["secure_key"]
-    return configuration
+    configuration.failed_url  = yaml["failed_url"]
+    configuration.secure_key  = yaml["secure_key"]
+    
+    configuration
   end
 
   def self.configure_from_rails
     path = ::Rails.root.join("config", "gopay.yml")
     configure_from_yaml(path) if File.exists?(path)
+    
     env = if defined?(::Rails) && ::Rails.respond_to?(:env)
             ::Rails.env.to_sym
           elsif defined?(::RAILS_ENV)
             ::RAILS_ENV.to_sym
           end
-    configuration.environment ||= (env == :development) ? :test : env
+    configuration.environment ||= (env == :production) ? :production : :test
+    
     warn "GoPay wasnt properly configured." if GoPay.configuration.goid.blank?
     configuration
   end
 
   class Configuration
     attr_accessor :environment, :goid, :success_url, :failed_url, :secure_key
-    attr_reader :country_codes, :messages
+    attr_reader   :country_codes, :messages
 
     def initialize
       @country_codes = YAML.load_file File.join(BASE_PATH, "config", "country_codes.yml")
-      config = YAML.load_file(File.join(BASE_PATH, "config", "config.yml"))
-      @urls = config["urls"]
+      config    = YAML.load_file(File.join(BASE_PATH, "config", "config.yml"))
+      @urls     = config["urls"]
       @messages = config["messages"]
     end
 
@@ -56,7 +66,5 @@ module GoPay
       env = @environment.nil? ? "test" : @environment.to_s
       @urls[env]
     end
-
   end
-
 end
