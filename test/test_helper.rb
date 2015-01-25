b67d6db1679d9d37dd247afa9b9b86fe92412dc6
Eda Riedl <eduardriedl@gmail.com>
2015-01-25 17:27:06 +0100

    Remove Ruby 1.8 test configuration.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,13 +3,7 @@ $: << File.join(File.expand_path(File.dirname(__FILE__)), "../lib")
 require "rubygems"
 require 'minitest/autorun'
 require "shoulda"
-
-if RUBY_VERSION > "1.9"
-  require 'mocha/setup'
-else
-  require 'mocha'
-end
-
+require 'mocha/setup'
 require "gopay"
 
 GoPay.configure do |config|
